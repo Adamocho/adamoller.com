146d2eab6b6990d125116f99bf304f270a25ca48
Adamocho <81510507+Adamocho@users.noreply.github.com>
2024-02-17 21:18:17 +0100

    Kind of finish the

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,17 +6,26 @@ const IndexPage: React.FC<PageProps> = () => {
   return (
     <main>
       <header>
-        <div>About</div>
+        <div>Portfolio</div>
         <div>Hobbys</div>
         <div>Contact</div>
       </header>
       <div className="about">
         <img src="../images/icon.png" alt="" />
-        <span className="about-name">Adam Oller</span>
-        <div className="about-separator"></div>
-        <span className="about-title">both learning & programming addict</span>
-        <span className="about-bio">A fast bio here! Don't look at me I'm a lorem ipsum. Kind of..</span>
-        <span className="signature">Adam Oller</span>
+        <div className="about-names">
+          <span className="about-name signature">Adam Oller</span>
+          <div className="about-separator"></div>
+          <p className="about-title">learning && programming addict</p>
+        </div>
+        <span className="about-bio">
+          Hello there! A young, aspiring software developer who loves 
+          exploring computer hardware and software from 
+          inside out is me. Throughout the years several talents emerged, so
+          obviously I have much to offer. Starting with Linux, through SVG
+          graphics and Android apps, ending with emulators written in Rust.
+          Non-exhaustive list of hobbies: film photography, calistenics, 
+          canoeing and books to (truthfuly) name a few. I'm hard-working 
+          and can't wait to hear from you soon!</span>
       </div>
       <div className="content">
         <h1 className="ornament">Hello there</h1>
