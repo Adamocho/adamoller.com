9541ca0d0397af545dd0502d141c730618e574d7
Adamocho <81510507+Adamocho@users.noreply.github.com>
2024-02-17 20:35:55 +0100

    Change html in 404 and index. Serve as backup

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -2,21 +2,15 @@ import * as React from "react"
 import { HeadFC, PageProps } from "gatsby"
 import "../styles/index.css"
 
-const pageStyles = {
-  backgroundColor: "#211919",
-  color: "#f4ebeb",
-  fontSize: "25px",
-  fontFamily: "Roboto, sans-serif, serif",
-}
-
 const NotFoundPage: React.FC<PageProps> = () => {
   return (
-    <main style={pageStyles}>
-      <h1>Not found. Sorry :C</h1>
+    <main className="not-found">
+      <h1>Error 404</h1>
+      <p>Sorry, not found. This page probably exists in another universe though :P</p>
     </main>
   )
 }
 
 export default NotFoundPage
 
-export const Head: HeadFC = () => <title>Not found</title>
+export const Head: HeadFC = () => <title>404 Not found</title>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,15 +2,26 @@ import * as React from "react"
 import type { HeadFC, PageProps } from "gatsby"
 import "../styles/index.css"
 
-const pageStyles = {
-  fontSize: "25px",
-}
-
 const IndexPage: React.FC<PageProps> = () => {
   return (
-    <main style={pageStyles}>
-      <h1>Adamocho</h1>
-      <p>Hello there! This is my personal website!</p>
+    <main>
+      <header>
+        <div>About</div>
+        <div>Hobbys</div>
+        <div>Contact</div>
+      </header>
+      <div className="about">
+        <img src="../images/icon.png" alt="" />
+        <span className="about-name">Adam Oller</span>
+        <div className="about-separator"></div>
+        <span className="about-title">both learning & programming addict</span>
+        <span className="about-bio">A fast bio here! Don't look at me I'm a lorem ipsum. Kind of..</span>
+        <span className="signature">Adam Oller</span>
+      </div>
+      <div className="content">
+        <h1 className="ornament">Hello there</h1>
+      </div>
+      <footer>Copyright Adam Oller &#169; 2024 All rights reserved</footer>
     </main>
   )
 }
