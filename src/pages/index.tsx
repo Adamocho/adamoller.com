c2738231d0771e4279b7d28a7cabe6b763207e66
Adamocho <81510507+Adamocho@users.noreply.github.com>
2024-02-27 20:22:57 +0100

    Finish css for .about and footer

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,17 +5,19 @@ import "../styles/index.css"
 const IndexPage: React.FC<PageProps> = () => {
   return (
     <main>
-      <header>
-        <div>Portfolio</div>
-        <div>Hobbys</div>
-        <div>Contact</div>
-      </header>
+      <nav>
+        <div className="items">
+          <div>Portfolio</div>
+          <div>Hobbys</div>
+          <div>Contact</div>
+        </div>
+      </nav>
       <div className="about">
         <img src="../images/icon.png" alt="" />
         <div className="about-names">
           <span className="about-name signature">Adam Oller</span>
           <div className="about-separator"></div>
-          <p className="about-title">learning && programming addict</p>
+          <p className="about-title">Programmer and a jack of all trades</p>
         </div>
         <span className="about-bio">
           Hello there! A young, aspiring software developer who loves 
@@ -23,14 +25,51 @@ const IndexPage: React.FC<PageProps> = () => {
           inside out is me. Throughout the years several talents emerged, so
           obviously I have much to offer. Starting with Linux, through SVG
           graphics and Android apps, ending with emulators written in Rust.
-          Non-exhaustive list of hobbies: film photography, calistenics, 
-          canoeing and books to (truthfuly) name a few. I'm hard-working 
-          and can't wait to hear from you soon!</span>
+          A non-exhaustive list of hobbies: film photography, calistenics, 
+          canoeing and books to (truthfuly) name a few. I'm ready for new adventures and can't wait to hear from you soon!
+        </span>
       </div>
       <div className="content">
-        <h1 className="ornament">Hello there</h1>
+        <h1 className="">Hello there</h1>
+        <h1>SOMETHING</h1>
+        <h1>SOMETHING</h1>
+        <h1>SOMETHING</h1>
+        <h1>SOMETHING</h1>
+        <h1>SOMETHING</h1>
+        <h1>SOMETHING</h1>
+        <h1>SOMETHING</h1>
+        <h1>SOMETHING</h1>
+        <h1>SOMETHING</h1>
+        <h1>SOMETHING</h1>
+        <h1>SOMETHING</h1>
+        <h1>SOMETHING</h1>
+        <h1>SOMETHING</h1>
+        <h1>SOMETHING</h1>
+        <h1>SOMETHING</h1>
+        <h1>SOMETHING</h1>
+        <h1>SOMETHING</h1>
+        <h1>SOMETHING</h1>
+        <h1>SOMETHING</h1>
+        <h1>SOMETHING</h1>
+        <h1>SOMETHING</h1>
+        <h1>SOMETHING</h1>
+        <h1>SOMETHING</h1>
+        <h1>SOMETHING</h1>
+        <h1>SOMETHING</h1>
+        <h1>SOMETHING</h1>
+        <h1>SOMETHING</h1>
+        <h1>SOMETHING</h1>
+        <h1>SOMETHING</h1>
+        <h1>SOMETHING</h1>
+        <h1>SOMETHING</h1>
+        <h1>SOMETHING</h1>
+        <h1>SOMETHING</h1>
+        <h1>SOMETHING</h1>
+        <h1>SOMETHING</h1>
+        <h1>SOMETHING</h1>
+        <h1>SOMETHING</h1>
       </div>
-      <footer>Copyright Adam Oller &#169; 2024 All rights reserved</footer>
+      <footer>Copyright &#169; Adam Oller</footer>
     </main>
   )
 }
