e4f508b09302b672f9b0749f61ffb87fcbcdef6f
Adamocho <81510507+Adamocho@users.noreply.github.com>
2024-04-04 17:36:23 +0200

    Finish  page

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -6,7 +6,10 @@ const Contact: React.FC<PageProps> = () => {
     return (
         <div className="contact">
             <h1>Contact section</h1>
-            {/* <p>social links here</p> */}
+            <p>LinkedIn: <span className="contact-link">https://www.linkedin.com/in/olleradam</span></p>
+            <p>GitHub: <span className="contact-link">https://www.github.com/adamocho</span></p>
+            <p>Email: <span className="contact-link">contact@adamoller.com</span></p>
+            <p>Career: <span className="contact-link">career@adamoller.com</span></p>
         </div>
     )
 };
