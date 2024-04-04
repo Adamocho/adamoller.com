7fa7fff250ed91434b2edc0d926b190bca053c0b
Adamocho <81510507+Adamocho@users.noreply.github.com>
2024-04-04 17:36:37 +0200

    Finish  page

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -13,7 +13,9 @@ const Summary: React.FC<PageProps> = () => {
                 inside out. Throughout the years several talents emerged, such as 
                 love of lerning, reading and doing all kinds of sport activities. 
                 Starting with Linux, through SVG graphics and Android apps, 
-                ending with emulators written in Rust - he's seen it all.
+                ending with emulators written in Rust - he's seen it all. 
+                Follower of a minimalistic style - in projects and personal 
+                life keeping to the Ockham's razor principle, adapting when required. 
                 <br/>
                 A short list of hobbies: film photography, calistenics,
                 canoeing and books.
@@ -22,9 +24,10 @@ const Summary: React.FC<PageProps> = () => {
                     <p>Personal achievements:</p>
                     <ul>
                         <li>Became an official programmer (with a title) in 2023</li>
+                        <li>Advanced Linux user with a ton of experience paired with a gigantic knowledge about the system, commands, shell and more</li>
                         <li>52 books read in 2024</li>
                         <li>Can read at thrice the averege human reading speed</li>
-                        <li>Speaks fluently in Polish, English, German, plus a glimpse of Ukrainian/Russian</li>
+                        <li>Speaks fluently in Polish, English, German and a glimpse of Ukrainian/Russian</li>
                     </ul>
                 </div>
             </div>
